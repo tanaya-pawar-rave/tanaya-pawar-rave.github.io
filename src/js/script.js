1999479d394ce6082cbc94f1687b21a4b23341fb
tanaya pawar <tanaya.pawar@northgateps.com>
2020-09-29 14:59:01 +0530

    API data mapped with html page and styles added.

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,62 @@
 
-    // Creating the XMLHttpRequest object
-    var request = new XMLHttpRequest();
+    var emailStr = '';
+    var description = '';
+
+      function displayResult(){
+        $(".transparentCover").show();
+        $(".loading").show();
    
+         // Creating the XMLHttpRequest object
+         var request = new XMLHttpRequest();
+         
+        // Instantiating the request object
+        request.open("GET", "https://cors-anywhere.herokuapp.com/https://ltv-data-api.herokuapp.com/api/v1/records.json?email=doesmith@example.com");
 
-    // Instantiating the request object
-    request.open("GET", "https://cors-anywhere.herokuapp.com/https://ltv-data-api.herokuapp.com/api/v1/records.json?email=doesmith@example.com");
+        request.onload = function () {
+            // Begin accessing JSON data here
+
+            if (request.status >= 200 && request.status < 400) {
+                var data = JSON.parse(this.response);
+                emailStr = data.email;
+                sessionStorage.setItem("email", emailStr);
+                description = data.description;
+                sessionStorage.setItem("des", description);
+                var name = data.first_name + ' ' + data.last_name;
+                sessionStorage.setItem("name", name);
+                var phoneNo = data.phone_numbers;
+                sessionStorage.setItem("number", phoneNo);
+                var address = data.address;
+                sessionStorage.setItem("add", address);
+                var rel = data.relatives;
+                sessionStorage.setItem("relative", rel);
 
 
-    request.onload = function () {
-        // Begin accessing JSON data here
-        if (request.status >= 200 && request.status < 400) {
-             var data = JSON.parse(this.response);
-           
-              console.log(data);
-        
-          } else {
-            console.log('error')
-          }
-        }
+                var searchStr = document.getElementById('email').value;
+                if(searchStr == emailStr){
+                //  var url = "result.html?des=" + description;
+                $(".transparentCover").hide(); 
+                $(".loading").hide(); 
+                  location.href= "result.html";        
+               
+                }
+                else{
+                  $(".loading").hide(); 
+                    alert('error');
+                }
+            
+              } else {
+                $(".loading").hide(); 
+                console.log('error')
+              }
+            }
       
       
       // Send request
       request.send()
+      
+        
+        
+      }
 
-
-    
+      
   
